e067e15027af4fa00a57e55788b6a370191280e3
Secrefar <k.nazarova90@gmail.com>
2021-11-25 22:12:13 +0300

    solution 14

---
+++ Solution 14/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = new Random().Next(0, 999000);
+Console.Write("a = ");
+Console.WriteLine(a);
+
+a = a % 1000;
+
+if(a > 99)
+{
+     Console.Write("Third digit = ");
+    Console.Write((a / 100) % 10);
+}
+   else
+{
+    Console.WriteLine("N/A");
+}
