c93847c2937ef3ec630a22080a74c564a91c5124
Secrefar <k.nazarova90@gmail.com>
2021-11-25 21:46:19 +0300

    solution 13

---
+++ Solution 13/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Insert number b = ");
+int b = Int32.Parse(Console.ReadLine());
+int a = new Random().Next(1,100);
+
+Console.Write("a = ");
+Console.WriteLine(a);
+
+if(a % b == 0)
+{
+    Console.WriteLine("Divisible");
+}
+else
+{
+    Console.Write("surplus = ");
+    Console.WriteLine(a % b);
+}
