c77ff99a473aa9be15bac16f0ecd4484b4c075e0
Secrefar <k.nazarova90@gmail.com>
2021-11-25 21:08:24 +0300

    solution 11

---
+++ Solution 11/Program.cs
@@ -0,0 +1,22 @@
+﻿int a = new Random().Next(10,99);
+Console.Write("a = ");
+Console.WriteLine(a);
+
+int digit1 = a / 10;
+Console.Write("digit1 = ");
+Console.WriteLine(digit1);
+
+int digit2 = a % 10;
+Console.Write("digit2 = ");
+Console.WriteLine(digit2);
+
+if(digit1 > digit2)
+{
+    Console.Write("max = ");
+    Console.WriteLine(digit1);
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(digit2);
+}
