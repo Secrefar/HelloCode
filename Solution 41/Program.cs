89dff60cb4ac6f01103f80f0c692449a5dfd3f85
Secrefar <k.nazarova90@gmail.com>
2021-12-09 20:45:46 +0300

    solutions 41 and 42

---
+++ Solution 41/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine ("Enter number A");
+int A = int.Parse(Console.ReadLine ());
+
+Console.WriteLine ("Enter number B");
+int B = int.Parse(Console.ReadLine ());
+
+Console.WriteLine ("Enter number C");
+int C = int.Parse(Console.ReadLine ());
+
+if ( A + B > C && A + C > B && B + C > A) Console.WriteLine ("Yes!");
+else 
+{ 
+    Console.WriteLine("No!");
+}
---
+++ Solution 42/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine ("Enter a number");
+Int64 A = Int64.Parse (Console.ReadLine ());
+
+int count = 0;
+
+while (A > 0)
+{
+    A = A/10;
+    count++;
+    char [] array = A.ToCharArray();
+}
+
